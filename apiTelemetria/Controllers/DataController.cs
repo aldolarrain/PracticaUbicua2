17d6d978b568b5941eb333cab2edf16a88d70768
aldolarrain <a2016114029@estudiantes.upsa.edu.bo>
2021-04-18 18:16:05 -0400

    apitelemetria

---
+++ apiTelemetria/Controllers/DataController.cs
@@ -0,0 +1,42 @@
+﻿using apiTelemetria.Controllers.Models;
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiTelemetria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<bool> EnviarAsync([FromBody] Data data)
+        {
+
+            string connectionString = "Endpoint=sb://queuealdo.servicebus.windows.net/;SharedAccessKeyName=Enviar;SharedAccessKey=JtBQdG776rTy57FzgCL1vOdWevmnQ6UU+j2U8zGHWPo=;EntityPath=practica";
+            string queueName = "practica";
+            string mensaje = JsonConvert.SerializeObject(data);
+
+            // create a Service Bus client 
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+
+            return true;
+        }
+    }
+}
